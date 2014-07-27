1f8160dd80a5032846647d35a8340811c049619c
venam <patrick@unixhub.net>
2014-07-27 21:10:49 +0300

    updater with comments for python3

--- Updater.py
+++ Updater.py
@@ -1,4 +1,6 @@
 from urllib import URLopener
+#if python3
+#from urllib import request
 import time
 import md5
 
@@ -7,11 +9,15 @@ class Updater:
         self._server = server
         self._infoFile = infoFile
         self.br = URLopener()
+        #if python3
+        #self.br = request
 
     def hasNewInfo(self):
         f = open(self._infoFile,'r').read()
         m = md5.new(f).hexdigest()
         response = self.br.open(self._server+'/hash').read()
+        #if python3
+        #response = self.br.urlopen(self._server+'/hash').read()
         response = response.replace("\n","")
         print(response)
         print(m)
@@ -23,6 +29,8 @@ class Updater:
     def fetchNewInfo(self):
         print ("fetching Info")
         response = self.br.open(self._server+'/info.json').read()
+        #if python3
+        #response = self.br.urlopen(self._server+'/info.json').read()
         oldInfo = open(self._infoFile,'r').read()
         open(self._infoFile+"."+self.generateTimeStamp(),'w').write(oldInfo)
         open(self._infoFile,'w').write(response)
