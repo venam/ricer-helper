c1901fbdbf76a0afd741a64821c09e65045eba48
venam <patrick@unixhub.net>
2014-08-02 12:59:40 +0300

    html

--- plugins/html.py
+++ plugins/html.py
@@ -2,4 +2,29 @@ def getName():
     print ("html")
 
 def output(state,information):
-    print ("outputing in HTML")
+    categories = information.listCategories()
+    selections = state.selected
+    # testing the output by printing it to stdout 
+    print """
+<html>
+    <head>
+    </head>
+    <body>
+"""
+
+    for category in categories:
+        found = 0
+        for selection in selections:
+            if information.getCategory(selection) == category:
+                if state.comments[selection] :
+                    found += 1
+                    if found == 1:
+                        print( "\n</ul>\n<h2>" + category + "</h2>\n<ul>")
+                    print( "\n<li>\n<h3>"+ selection+"</h3><br/>")
+                    commentLines = state.comments[selection].split("\n")
+                    for line in commentLines :
+                        print (line+"<br/>")
+    print "</ul>"
+    print "</body>"
+    print "</html>"
+
